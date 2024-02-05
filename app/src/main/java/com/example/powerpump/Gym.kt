50650449753c6170ed2b668037dfd981546c05fd
133222 <Kyanogulik@gmail.com>
2024-02-05 14:29:33 +0100

    week1 gym workout af begin gym workout week2

--- app/src/main/java/com/example/powerpump/Gym.kt
+++ app/src/main/java/com/example/powerpump/Gym.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.Navigation
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -34,7 +36,11 @@ class Gym : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_gym, container, false)
+        val view = inflater.inflate(R.layout.fragment_gym, container, false)
+        view.findViewById<Button>(R.id.week1Gym).setOnClickListener {
+            Navigation.findNavController(view).navigate(R.id.action_gym_to_gymweek1)
+        }
+        return view
     }
 
     companion object {
